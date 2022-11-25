b69710e4049dd6d9742bc5b876a338fbf9522bc7
Sbipul <bipulcroy121@gmail.com>
2022-11-25 22:40:38 +0600

    async await concept revised

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
+// Promise call
+
+// const myPromise = new Promise((resolve, reject) => {
+//   const user = {id:1};
+//   if (!user) {
+//     reject("Not found user");
+//   } else {
+//     setTimeout(()=>{
+//         resolve({name:"john"});
+//     },1000)
+//   }
+// });
+// const userData = []
+// const userid = [1,2,3,4,5];
+// for (let i = 0; i < userid.length; i++) {
+//     const id = userid[i];
+//     userData.push(myPromise)
+// }
+// Promise.all(userData).then(res => console.log(res))
+// myPromise
+//   .then((res) => console.log("found in then", res))
+//   .catch((err) => console.log("found in catch", err));
+
+// Async await
+
 const myPromise = new Promise((resolve, reject) => {
-  const user = {id:1};
-  if (!user) {
-    reject("Not found user");
-  } else {
-    setTimeout(()=>{
-        resolve({name:"john"});
-    },1000)
-  }
+  setTimeout(() => {
+    resolve("success");
+  }, 1000);
 });
-const userData = []
-const userid = [1,2,3,4,5];
-for (let i = 0; i < userid.length; i++) {
-    const id = userid[i];
-    userData.push(myPromise)
-}
-Promise.all(userData).then(res => console.log(res))
-myPromise
-  .then((res) => console.log("found in then", res))
-  .catch((err) => console.log("found in catch", err));
+
+const getData = async () => {
+  const res = await myPromise;
+  console.log(res);
+};
+getData();
+
+fetch(url)
+  .then((res) => res.json)
+  .then((data) => console.log(data));
+
+const getUsers = async () => {
+  const res = await fetch("url");
+  const data = res.json();
+  console.log(data);
+
+  const res2 = await fetch("url");
+  const data2 = res.json();
+  console.log(data);
+};
+
+getUsers();
